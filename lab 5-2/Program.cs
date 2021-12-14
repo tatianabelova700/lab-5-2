497e343e567b72712ece10cf534fe6e2ba73d53a
Tatyana <Tatyana@DESKTOP-74DNP3C>
2021-12-14 21:15:57 +0300

    Добавьте файлы проекта.

---
+++ lab 5-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4. 5. Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN следующего вида:
+
+           1 0 1 0 1
+
+           0 1 0 1 0
+
+           1 0 1 0 1
+
+           0 1 0 1 0
+
+           1 0 1 0 1
+           */
+            Console.WriteLine("Введите количество N строк и столбцов массива ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[N, N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ lab 5-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab 5-2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab 5-2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("820d02ce-4d89-4407-93bc-1f2aa16527c0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
